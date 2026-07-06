b5057f3b98225c38eb85276e3109494b5b757333
Legend_7ok <dimtkac777@gmail.com>
2026-07-06 21:24:20 +0300

    feat(icons): add link icon

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -372,6 +372,17 @@ const Icons = {
       }
     />
   ),
+  link: (p) => (
+    <Ic
+      {...p}
+      d={
+        <>
+          <path d="M10 13a5 5 0 0 0 7 0l2-2a5 5 0 0 0-7-7l-1 1" />
+          <path d="M14 11a5 5 0 0 0-7 0l-2 2a5 5 0 0 0 7 7l1-1" />
+        </>
+      }
+    />
+  ),
 };
 
 export default Icons;
