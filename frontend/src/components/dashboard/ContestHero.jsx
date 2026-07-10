82a52be94ed9acff405f8d0222a37ca2e5579cfc
Legend_7ok <dimtkac777@gmail.com>
2026-07-10 16:44:27 +0300

    feat(contests): link the hero ctas to the contest page

--- frontend/src/components/dashboard/ContestHero.jsx
+++ frontend/src/components/dashboard/ContestHero.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Icons from '../Icons';
 import { useContestHero } from '../../hooks/useContestHero';
 import { joinContest, leaveContest } from '../../api/contests';
@@ -152,9 +153,9 @@ function LiveHero({ contest, standing, now }) {
           </div>
         </div>
         <div className="hero-cta">
-          <a className="btn btn-primary" href="#">
+          <Link className="btn btn-primary" to={`/contests/${contest.id}`}>
             <I.play size={15} /> Enter round
-          </a>
+          </Link>
         </div>
       </div>
     </section>
@@ -248,9 +249,9 @@ function SoonHero({ contest, onChanged, now }) {
               <I.flag size={15} /> Register
             </button>
           )}
-          <a className="btn" href="#">
+          <Link className="btn" to={`/contests/${contest.id}`}>
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </section>
@@ -271,9 +272,9 @@ function NoContest() {
       <div className="hero-foot">
         <div className="hero-stand" />
         <div className="hero-cta">
-          <a className="btn btn-primary" href="#">
+          <Link className="btn btn-primary" to="/contests">
             <I.trophy size={15} /> Browse contests
-          </a>
+          </Link>
         </div>
       </div>
     </section>
