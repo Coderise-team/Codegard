4b9df63a709ce683a7f6fc42756e78cb4519b06c
Legend_7ok <dimtkac777@gmail.com>
2026-08-04 14:14:44 +0300

    feat(shell): draw the sidebar user card avatar

--- frontend/src/components/layout/Sidebar.test.jsx
+++ frontend/src/components/layout/Sidebar.test.jsx
@@ -4,19 +4,30 @@ import { MemoryRouter } from 'react-router-dom';
 
 import Sidebar from './Sidebar';
 
+const renderSidebar = (user) =>
+  render(
+    <MemoryRouter>
+      <Sidebar user={user} onClose={() => {}} />
+    </MemoryRouter>
+  );
+
 describe('Sidebar', () => {
   it('sends the footer card to the signed-in user profile', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <Sidebar
-          user={{ username: 'n3ptune', initials: 'N3' }}
-          onClose={() => {}}
-        />
-      </MemoryRouter>
-    );
+    const { container } = renderSidebar({ username: 'n3ptune', avatar: null });
 
     expect(container.querySelector('.nav-mini').getAttribute('href')).toBe(
       '/users/n3ptune'
+    );
+  });
+
+  it('shows the picture of a user who has one', () => {
+    const { container } = renderSidebar({
+      username: 'n3ptune',
+      avatar: '/media/avatars/thumbs/abc.webp',
+    });
+
+    expect(container.querySelector('.avatar-img').getAttribute('src')).toBe(
+      '/media/avatars/thumbs/abc.webp'
     );
   });
 });
